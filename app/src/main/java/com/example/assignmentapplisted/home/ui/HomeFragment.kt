f2b95e14dd9768aa7bc335e9f1cbdae3c7fa101f
tahir <tahir19.fm@gmail.com>
2023-05-23 14:00:17 +0530

    ui and api call

--- app/src/main/java/com/example/assignmentapplisted/home/ui/HomeFragment.kt
+++ app/src/main/java/com/example/assignmentapplisted/home/ui/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.assignmentapplisted.home.ui
 
 import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.DashPathEffect
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,11 +18,24 @@ import com.example.assignmentapplisted.databinding.FragmentHomeBinding
 import com.example.assignmentapplisted.home.data.OpenInDAO
 import com.example.assignmentapplisted.home.util.HomeScreenAdapter
 import com.example.assignmentapplisted.home.util.HomeViewModel
+import com.example.assignmentapplisted.home.util.MyMarkerView
 import com.example.assignmentapplisted.network.ApiResult
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IFillFormatter
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import com.github.mikephil.charting.utils.Utils
 
 class HomeFragment : Fragment() {
     private val binding by lazy { FragmentHomeBinding.inflate(layoutInflater) }
     private val viewModel : HomeViewModel by activityViewModels()
+    private lateinit var chart: LineChart
     companion object{
         const val RECENT_LINKS=1
         const val TOP_LINKS=2
@@ -39,6 +54,7 @@ class HomeFragment : Fragment() {
     }
 
 
+    @SuppressLint("SuspiciousIndentation")
     private fun setupViews() {
     binding.rvLinksData.layoutManager=LinearLayoutManager(requireContext())
         binding.apply {
@@ -50,7 +66,9 @@ class HomeFragment : Fragment() {
                 viewModel.setState(TOP_LINKS)
             }
         }
+        setupGraph()
     }
+
 
     private fun setUpObserver() {
     viewModel.data.observe(viewLifecycleOwner){
@@ -97,6 +115,140 @@ class HomeFragment : Fragment() {
             tvTopLinks.setBackgroundResource(R.drawable.link_btn_res)
             tvRecentLinks.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
             tvRecentLinks.setBackgroundResource(R.color.transparent_color)
+        }
+    }
+
+
+    private fun setupGraph() {
+        chart=binding.chart
+        run {
+            // background color
+            chart.setBackgroundColor(Color.WHITE)
+
+            // disable description text
+            chart.description.isEnabled = false
+
+            // enable touch gestures
+            chart.setTouchEnabled(false)
+
+            // set listeners
+            val mv = MyMarkerView(requireContext(), R.layout.custom_marker_view)
+            mv.chartView = chart
+            chart.marker = mv
+
+            // enable scaling and dragging
+            chart.isDragEnabled = true
+            chart.setScaleEnabled(true)
+            chart.setPinchZoom(true)
+        }
+
+        val xAxis: XAxis = chart.xAxis
+
+        var yAxis: YAxis
+        run {   // // Y-Axis Style // //
+            yAxis = chart.axisLeft
+
+            // disable dual axis (only use LEFT axis)
+            chart.axisRight.isEnabled = false
+
+            // horizontal grid lines
+            yAxis.enableGridDashedLine(10f, 10f, 0f)
+
+            // axis range
+            yAxis.axisMaximum = 200f
+            yAxis.axisMinimum = -10f
+        }
+        val months = arrayOf("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+        xAxis.valueFormatter = IndexAxisValueFormatter(months)
+        xAxis.labelCount = months.size // Set the label count to the number of months
+        xAxis.setAvoidFirstLastClipping(true) // Avoid clipping of the first and last labels
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+
+        chart.setExtraOffsets(10f, 0f, 10f, 10f) // Add extra offset to accommodate the labels
+        chart.xAxis.textSize = 10f
+
+        xAxis.setCenterAxisLabels(true) // Center the labels between the grid lines
+        xAxis.granularity = 1f // Display one label per interval
+       // xAxis.labelRotationAngle = -45f // Rotate the labels by -45 degrees for better visibility
+
+        setData(8,9f)
+        chart.animateX(1500)
+
+        val legend = chart.legend
+        legend.form = Legend.LegendForm.LINE
+
+        chart.invalidate()
+
+    }
+    @SuppressLint("UseCompatLoadingForDrawables")
+    fun setData(count: Int, range: Float) {
+        val dataPoints = listOf(50f, 80f, 120f, 90f, 110f, 70f, 100f, 130f)
+        val values = ArrayList<Entry>()
+        for (i in dataPoints.indices) {
+            val value = dataPoints[i]
+            val entry = Entry(i.toFloat(), value,resources.getDrawable(R.drawable.fade_red))
+            values.add(entry)
+        }
+        val set1: LineDataSet
+        if (chart.data != null &&
+            chart.data.dataSetCount > 0
+        ) {
+            set1 = chart.data.getDataSetByIndex(0) as LineDataSet
+            set1.values = values
+            set1.notifyDataSetChanged()
+            chart.data.notifyDataChanged()
+            chart.notifyDataSetChanged()
+        } else {
+            // create a dataset and give it a type
+            set1 = LineDataSet(values, "")
+            set1.setDrawIcons(false)
+
+            // draw dashed line
+            set1.enableDashedLine(10f, 5f, 0f)
+
+            // black lines and points
+            set1.color = Color.BLACK
+            set1.setCircleColor(Color.BLACK)
+
+            // line thickness and point size
+            set1.lineWidth = 1f
+            set1.circleRadius = 3f
+
+            // draw points as solid circles
+            set1.setDrawCircleHole(false)
+
+            // customize legend entry
+            set1.formLineWidth = 1f
+            set1.formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
+            set1.formSize = 15f
+
+            // text size of values
+            set1.valueTextSize = 9f
+
+            // draw selection line as dashed
+            set1.enableDashedHighlightLine(10f, 5f, 0f)
+
+            // set the filled area
+            set1.setDrawFilled(true)
+            set1.fillFormatter =
+                IFillFormatter { dataSet, dataProvider -> chart.axisLeft.axisMinimum }
+
+            // set color of filled area
+            if (Utils.getSDKInt() >= 18) {
+                // drawables only supported on api level 18 and above
+                val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.fade_red)
+                set1.fillDrawable = drawable
+            } else {
+                set1.fillColor = Color.BLACK
+            }
+            val dataSets = ArrayList<ILineDataSet>()
+            dataSets.add(set1) // add the data sets
+
+            // create a data object with the data sets
+            val data = LineData(dataSets)
+
+            // set data
+            chart.data = data
         }
     }
 
---
+++ app/src/main/java/com/example/assignmentapplisted/home/util/MyMarkerView.kt
@@ -0,0 +1,34 @@
+package com.example.assignmentapplisted.home.util
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.widget.TextView
+import com.example.assignmentapplisted.R
+import com.github.mikephil.charting.components.MarkerView
+import com.github.mikephil.charting.data.CandleEntry
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.utils.MPPointF
+import com.github.mikephil.charting.utils.Utils
+
+
+@SuppressLint("ViewConstructor")
+class MyMarkerView(context: Context?, layoutResource: Int) :
+    MarkerView(context, layoutResource) {
+    private val tvContent: TextView = findViewById<TextView>(R.id.tvContent)
+
+    // runs every time the MarkerView is redrawn, can be used to update the
+    // content (user-interface)
+    override fun refreshContent(e: Entry, highlight: Highlight) {
+        if (e is CandleEntry) {
+            tvContent.text = Utils.formatNumber(e.high, 0, true)
+        } else {
+            tvContent.text = Utils.formatNumber(e.y, 0, true)
+        }
+        super.refreshContent(e, highlight)
+    }
+
+    override fun getOffset(): MPPointF {
+        return MPPointF(-(width / 2).toFloat(), -height.toFloat())
+    }
+}
